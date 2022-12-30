6f7db01968ea2feb01727502d64e366e1b9d2f5a
MaroClay155 <amarwan744@gmail.com>
2022-12-30 14:55:03 +0200

    Adding 3d OSM Maps to unity

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody camera;
+    private Vector2 move;
+    private Vector2 turn;
+    //private float zoom;
+    //private Vector3 deltamove;
+    //private float sensetivity;
+    
+
+    void Start()
+    {
+        camera = GetComponent<Rigidbody>();
+        //Cursor.lockState = CursorLockMode.Locked;//hide cursor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+        camera.velocity = new Vector3((40 * move.x), 0, (40 * move.y));
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler((-turn.y * 5), ( turn.x * 5), 0);
+        /*
+        if(Input.GetKeyDown == u)
+        {
+            zoom += (float) u;
+            camera.velocity = new Vector3(camera.velocity.x, 0, camera.velocity.y);
+        }
+        */
+
+    }
+}
