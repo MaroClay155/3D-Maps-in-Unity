a23ed1c1c6d6357afe01767a14e8498d8256ba0c
Marwan Waheed <104381945+MaroClay155@users.noreply.github.com>
2022-12-31 19:15:16 +0200

    Update Move.cs

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -17,7 +17,6 @@ public class Move : MonoBehaviour
     private bool click, clickflag = false;
     void Start()
     {
-
         //MyInput();
         //CameraMovement();
         cameraobject = GetComponent<Rigidbody>();
