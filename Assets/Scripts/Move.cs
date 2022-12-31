0fc199fed62d0e6b7ddf9d7e4d304bdc8a1e1452
Marwan Waheed <104381945+MaroClay155@users.noreply.github.com>
2022-12-31 11:44:06 +0200

    Updating Camera Controls

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,37 +1,73 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Rigidbody myCamera;
-    private Vector2 move;
+    private Rigidbody cameraobject;
+    private Vector3 moveDirection;
+    private float horizontalInput;
+    private float verticalInput;
+    public Transform orientation;
     private Vector2 turn;
-    
+    //private float field_of_view = 60f;//normal
+    private float mouseScrollWheel;
+    //private Vector2 move;
 
     void Start()
     {
-        myCamera = GetComponent<Rigidbody>();
+
+        //MyInput();
+        //CameraMovement();
+        cameraobject = GetComponent<Rigidbody>();
         //Cursor.lockState = CursorLockMode.Locked;//hide cursor
     }
-
+    private void FixedUpdate()
+    {
+        CameraMovement();
+    }
     // Update is called once per frame
     void Update()
     {
-        move.x = Input.GetAxis("Horizontal");
-        move.y = Input.GetAxis("Vertical");
-        myCamera.velocity = new Vector3((40 * move.x), 0, (40 * move.y));
+        MyInput();
+        //myCamera.velocity = new Vector3((40 * move.x), 0, (40 * move.y));
+        transform.localRotation = Quaternion.Euler((-turn.y * 3), ( turn.x * 3), 0);
+
+
+        //if (Input.GetKey(KeyCode.Z))
+        //{ field_of_view -= 20f; }
+        //if (Input.GetKey(KeyCode.X))
+        //{ field_of_view += 20f; }
+        //GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, field_of_view, Time.deltaTime * 5);
+
+        if(mouseScrollWheel > 0)
+        {
+            //GetComponent<Camera>().fieldOfView--;
+            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y - 8.0f, transform.position.z);
+            //transform.Rotate(-1,0,0);        
+        }
+
+        if (mouseScrollWheel < 0)
+        {
+            //GetComponent<Camera>().fieldOfView++;
+            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y + 8.0f, transform.position.z);
+            //transform.Rotate(1, 0, 0);
+        }
+    }
+
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         turn.x += Input.GetAxis("Mouse X");
         turn.y += Input.GetAxis("Mouse Y");
-        transform.localRotation = Quaternion.Euler((-turn.y * 5), ( turn.x * 5), 0);
-
-        
-        if(Input.GetKey(KeyCode.Z))
-            myCamera.velocity = new Vector3(myCamera.velocity.x, 40, myCamera.velocity.y);
-
-        if (Input.GetKey(KeyCode.X))
-            myCamera.velocity = new Vector3(myCamera.velocity.x, -40, myCamera.velocity.y);
-
+        mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
+    }
+    void CameraMovement()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        //cameraobject.AddForce(moveDirection.normalized * 40f, ForceMode.Force);
+        cameraobject.velocity = moveDirection * 50;
     }
 }
