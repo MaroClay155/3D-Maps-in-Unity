ddf4cbd6a67234d9a3e00a984c2d4307623cb74c
Marwan Waheed <104381945+MaroClay155@users.noreply.github.com>
2023-01-01 19:18:04 +0200

    Update RawImagefromByteArray.cs

--- Assets/Scripts/RawImagefromByteArray/RawImagefromByteArray.cs
+++ Assets/Scripts/RawImagefromByteArray/RawImagefromByteArray.cs
@@ -5,18 +5,11 @@ using UnityEngine.UI;
 
 public class sinewave : MonoBehaviour
 {
-    //public RawImage image;
-
-    // Start is called before the first frame update
     void Start()
     {
         // Create a 16x16 texture with PVRTC RGBA4 format
         // and fill it with raw PVRTC bytes.
         Texture2D tex = new Texture2D(16, 16, TextureFormat.PVRTC_RGBA4, false);
-        // Raw PVRTC4 data for a 16x16 texture. This format is four bits
-        // per pixel, so data should be 16*16/2=128 bytes in size.
-        // Texture that is encoded here is mostly green with some angular
-        // blue and red lines.
         byte[] pvrtcBytes = new byte[]
         {
             0x30, 0x32, 0x32, 0x32, 0xe7, 0x30, 0xaa, 0x7f, 0x32, 0x32, 0x32, 0x32, 0xf9, 0x40, 0xbc, 0x7f,
@@ -28,11 +21,8 @@ public class sinewave : MonoBehaviour
             0x00, 0x00, 0x00, 0xff, 0xe6, 0x40, 0x01, 0x2c, 0x00, 0xff, 0x00, 0xaa, 0xdb, 0x41, 0xff, 0xff,
             0x00, 0x00, 0x00, 0xff, 0xe8, 0x40, 0x01, 0x1c, 0x00, 0xff, 0x00, 0xaa, 0xbb, 0x40, 0xff, 0xff,
         };
-        // Load data into the texture and upload it to the GPU.
         tex.LoadRawTextureData(pvrtcBytes);
         tex.Apply();
-        // Assign texture to renderer's material.
-        //GetComponent<Renderer>().material.mainTexture = tex; // works for a 3D-Object
         GetComponent<RawImage>().material.mainTexture = tex; // works for Raw Image
     }
 }
